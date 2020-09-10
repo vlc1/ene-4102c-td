e73239302561742d46f568e6a77fbf15c51986c8
Vincent Le Chenadec <vincentlechenadec@gmail.com>
2020-09-10 14:40:09 +0200

    Part 2 of TD1 added

---
+++ td1-part2.jl
@@ -0,0 +1,33 @@
+### A Pluto.jl notebook ###
+# v0.11.14
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ d3f9af78-f35e-11ea-0812-1b1069364fba
+md"""
+# Exercice 1
+
+Établir pour chacune des fonctions proposées ci-dessous un développement limité en $0$ à l'ordre $n$.
+
+| $f$ | $n$ |
+|:--------:|:-----:|
+| $$x \mapsto \frac{\ln \left ( 1 + x \right )}{1 + x}$$ | $3$ |
+| $$x \mapsto \frac{\ln \left [ \cosh \left ( x \right ) \right ]}{x \ln \left ( 1 + x \right )}$$ | $2$ |
+| $$x \mapsto \frac{\cos \left ( x \right ) - 1}{\ln \left ( 1 + x \right ) \sinh \left ( x \right )}$$ | $3$ |
+
+"""
+
+# ╔═╡ 07859f52-f360-11ea-0540-893a89f816ff
+md"""
+# Exercice 2
+
+Calculer les limites suivantes.
+
+$$\lim_{x \to 0} \frac{\sinh \left ( x \right )}{\sin \left ( x \right )}$$
+
+"""
+
+# ╔═╡ Cell order:
+# ╟─d3f9af78-f35e-11ea-0812-1b1069364fba
+# ╟─07859f52-f360-11ea-0540-893a89f816ff
